b79a057d1cff19f35cafc003546d50ce0877f3ec
Ashish <ad1358787@gmail.com>
2023-08-06 17:44:52 +0530

    some update

--- Lecture2_eventloop_apifirst_day/2.1/index.js
+++ Lecture2_eventloop_apifirst_day/2.1/index.js
@@ -1,16 +1,17 @@
-const fs = require("fs");
+const fs = require("fs");  //fs is file system module which comes with node while installing it ecause browser can't handle it.
+                          // this allows us to Read and write file
 
 function someAsyncOperation(callback) {
-  fs.readFile("abc.txt", callback);
+  fs.readFile("abc.txt", callback);    // fs.readfile is async operation and iy will be moved to the call back queue.
 }
 
-const timeoutScheduled = Date.now(); // 1646301758201
+const timeoutScheduled = Date.now(); // 1646301758201  it's give current time in milisecond from January1,1970
 console.log("timeoutScheduled", timeoutScheduled);
 
 setTimeout(() => {
   console.log("now", Date.now());
   const delay = Date.now() - timeoutScheduled;
-  console.log(`${delay}ms have passed since I was scheduled`);
+  console.log(`${delay}ms have passed since I was scheduled`);  // it will be moved to timer queue
 }, 100);
 
 // do someAsyncOperation which takes 95 ms to complete
@@ -23,3 +24,10 @@ const callbackFunction = () => {
   }
 };
 someAsyncOperation(callbackFunction);
+
+// here is explanation ----> line no first will be put into memory and line no 4 will be put into the memory as it is function defination
+// and line no 8 will be put into memory after that setTimeout will be pushed into the timer queue then call back function is function defination then it is moved to memory. Now while execution time all thing willbe executed line by line except asynchronuos thing 
+ // now here is catch if timer queue and call back queue content is same than callback queue content 
+ // will be pushed first in the Ready queue than call back queue. when call back is Ready then then it will be moved into then call stack after executing it it will be removed from the stack and for the setTimeout as well 
+ // when Ready queue will be empty then only node will check for the timer queue or call back queue.
+ 
--- Lecture2_eventloop_apifirst_day/2.2/index.js
+++ Lecture2_eventloop_apifirst_day/2.2/index.js
@@ -1,6 +1,16 @@
-const express = require("express");
+//  for every node JS application we have to do---> npm init -y, and then installation of express by running command---> npm i express, and nodemon insatelled globally for this command is---> npm install -g nodemon.
+
+const express = require("express");   // express is function which runs after invoking it
+
+// Express is a Fast, unopinionated, minimalist web framework for Node.js. here unopinionated means it don't provide the boiler code . we can create folder structure as our own wish 
+
+
+
+// console.log(express);
 
 const app = express();
+
+
 
 // REST APIs => Representational State Transfer
 /*
@@ -10,14 +20,32 @@ const app = express();
     delete => remove some data from server
 */
 
+// express is always looking for method+route
+
+// get is a function which takes two parameter first is route and second is call back function whihc takes two parameter
+// request and responce . and here responce is an object on which send is a function.
+
 app.get("/users", function (req, res) {
   res.send({ name: "Dhaval" });
 });
 
 app.get("/students", function (req, res) {
+  // console.log(req.method) to check the methods
   res.send({ student: "Abc" });
 });
 
 app.listen(4000, () => {
   console.log("listening on port 4000");
 });
+
+// it is advised use port between 2000 to 10000
+
+
+// server ---> A server is a computer program or device which is put on centeral location and their task to send the responce based the request send by the clients(computer). A server can give responce to muiltiple client and in same way clients can use multple server for responce.
+// a port is logical adddress which indentifies a specific service on computer.
+
+// lets understand By this example ----> when postman come with lettter aks the name of arif then here arif is a port and address of that home is localhost of which every member is having a same address.
+
+// to check responce in the postman do like this --->  http://localhost:4000/route(users/studdent)
+
+// https://opportunities.masaischool.com/placements till .com is ia domain after that is a route.
